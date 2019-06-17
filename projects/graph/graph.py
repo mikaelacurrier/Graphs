d0770137ae772d63f225ad453a7d272ef2510555
mikaelacurrier <mikaela.currier@gmail.com>
2019-06-17 18:47:07 -0400

    fixed spelling error and added error flag to adding edge

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -12,19 +12,21 @@ class Graph:
         Add a vertex to the graph.
         """
         self.vertices[vertex] = set()
-\    def add_edge(self, v1, v2):
+    def add_edge(self, v1, v2):
         """
         Add a directed edge to the graph.
         """
         if v1 in self.vertices and v2 in self.vertices:
             self.vertices[v1].add(v2)
+        else:
+            raise IndexError('Vertex does not exist.')
 
     def bft(self, starting_vertex):
         """
         Print each vertex in breadth-first order
         beginning from starting_vertex.
         """
-        visted = set()
+        visited = set()
         q = Queue()
         q.enqueue(starting_vertex)
 
@@ -41,7 +43,7 @@ class Graph:
         Print each vertex in depth-first order
         beginning from starting_vertex.
         """
-        visted = set()
+        visited = set()
         s = Stack()
         s.push(starting_vertex)
         while s.size() > 0:
@@ -65,7 +67,8 @@ class Graph:
         starting_vertex to destination_vertex in
         breath-first order.
         """
-        pass  # TODO
+      pass
+      
     def dfs(self, starting_vertex, destination_vertex):
         """
         Return a list containing a path from
